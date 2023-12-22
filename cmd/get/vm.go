04353b480359fc8749ee96414f2f5c9a0abaf227
prot0s34 <nikprotosmas@yandex.ru>
2023-12-23 01:13:38 +0300

    add fields to vm.go get func

--- cmd/get/vm.go
+++ cmd/get/vm.go
@@ -29,7 +29,7 @@ var vmCmd = &cobra.Command{
 		// string-by-string copying output of powershell looks little ugly.
 		// Strongly needed to redesign that part, but call to winrm for hyperv communitation broken by design in that case. %)
 		var stdout, stderr bytes.Buffer
-		command := "powershell -Command \"Get-VM | Format-Table -Property Name, State, Status -AutoSize | Out-String -Width 4096\""
+		command := "powershell -Command \"Get-VM | Format-Table -Property Name, State, CPUUsage, MemoryAssigned, Uptime, Status -AutoSize | Out-String -Width 4096\""
 		_, err = client.RunWithContext(ctx, command, &stdout, &stderr)
 		if err != nil {
 			fmt.Println("Error running command:", err)
